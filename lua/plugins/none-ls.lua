f1c930a96cc24f5aa34e70969606e1734d7f6c38
Dawid Rosinski <dawid.rosinski@danfoss.com>
2024-03-26 16:03:07 +0000

    updated after testing on windows PC, added surround plugin

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,8 +4,17 @@ return {
         local null_ls = require("null-ls")
 
         null_ls.setup({
+          sources = {
             null_ls.builtins.formatting.stylua, -- make sure to add it in :Mason
-            null_ls.builtins.diagnostics.pylint
+            -- null_ls.builtins.diagnostics.pylint.with({
+            --   diagnostics_postprocess = function(diagnostic)
+            --     diagnostic.code = diagnostic.message_id
+            --   end,
+            -- }),
+            -- null_ls.builtins.formatting.isort,
+            null_ls.builtins.diagnostics.pylint,
+            null_ls.builtins.formatting.black,
+          },
         })
 
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
