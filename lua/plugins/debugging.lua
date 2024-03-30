40f5353dc01c055257c75f76495e04f7739209ac
Dawid Rosiński <dawid.rosinski@gmail.com>
2024-03-30 11:11:04 +0000

    Added cpp debugging and git plugin

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,54 +1,99 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "mfussenegger/nvim-dap-python",
-        "mfussenegger/nvim-dap",
-        "nvim-neotest/nvim-nio"
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"mfussenegger/nvim-dap-python",
+		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-        dapui.setup()
-        -- require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+		dapui.setup()
+		-- require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+        require("dap-python").setup(vim.fn.getcwd() .. "/venv/bin/python")
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				-- CHANGE THIS to your path!
+				-- command = "/absolute/path/to/codelldb/extension/adapter/codelldb",
+                command = "/Users/dawid/.local/share/nvim/mason/bin/codelldb",
+				args = { "--port", "${port}" },
 
-        vim.keymap.set('n', '<F5>', function() dap.continue() end)
-        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-        vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-        vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
-        vim.keymap.set('n', '<Leader>lp',
-            function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-        vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-        vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-        vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-            require('dap.ui.widgets').hover()
-        end)
-        vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-            require('dap.ui.widgets').preview()
-        end)
-        vim.keymap.set('n', '<Leader>df', function()
-            local widgets = require('dap.ui.widgets')
-            widgets.centered_float(widgets.frames)
-        end)
-        vim.keymap.set('n', '<Leader>ds', function()
-            local widgets = require('dap.ui.widgets')
-            widgets.centered_float(widgets.scopes)
-        end)
-    end
+				-- On windows you may have to uncomment this:
+				-- detached = false,
+			},
+		}
+
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end)
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end)
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>B", function()
+			dap.set_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>lp", function()
+			require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end)
+		vim.keymap.set("n", "<Leader>dr", function()
+			dap.repl.open()
+		end)
+		vim.keymap.set("n", "<Leader>dl", function()
+			dap.run_last()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+			require("dap.ui.widgets").hover()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+			require("dap.ui.widgets").preview()
+		end)
+		vim.keymap.set("n", "<Leader>df", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.frames)
+		end)
+		vim.keymap.set("n", "<Leader>ds", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.scopes)
+		end)
+	end,
 }
